b068d262a9b72f7ad483b295e35de7fa3acc64b9
abiydoni <abiydoni@gmail.com>
2025-02-03 12:56:00 +0700

    Update service.php

--- service.php
+++ service.php
@@ -9,7 +9,6 @@
 					<div class="col-md-4 col-sm-12 col-xs-12">
 						<div class="section-title">
 						<h1><span><a href="scoring/index.php"><i class="fa fa-bullseye"></i> Scoring Apps</a></span> </h1><br>
-						<h1><span><a href="scoring/index.php"><i class="fa fa-bullseye"></i> Scoring Apps</a></span> </h1><br>
 						<h2>My <span>Service</span></h2>
 						</div> 
 					</div>
