9f1e41c7a5a1a7e753319c3255b265f16e83d41a
abiydoni <abiydoni@gmail.com>
2025-02-04 14:37:27 +0700

    Update video.php

--- video.php
+++ video.php
@@ -1,8 +1,8 @@
 <?php include 'header.php'; ?>
 <title>Video</title>
 
-<section id="about" class="about py-5">
-    <div class="container">
+<section id="about" class="service section">
+    <div class="container ">
         <div class="row text-center mb-4">
             <div class="col-12">
                 <h1 class="fw-bold">VIDEO</h1>
@@ -20,9 +20,9 @@
                     $video_url = str_replace("watch?v=", "embed/", $video_url);  // Ubah URL YouTube ke embed
                 }
             ?>
-                <div class="col-md-4 col-sm-6">
+                <div class="col-md-4 col-sm-6 col-xs-12"> <!-- Responsif untuk perangkat kecil -->
                     <div class="card shadow-sm border-0">
-                        <div class="ratio ratio-16x9">
+                        <div class="embed-responsive embed-responsive-16by9">
                             <!-- Menampilkan video embed langsung -->
                             <iframe width="100%" height="315" src="<?php echo $video_url; ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
