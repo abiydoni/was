f61be0232b3c49fbd3534929674d4b34359c4cc4
abiydoni <abiydoni@gmail.com>
2025-02-07 17:10:47 +0700

    hapus

--- js/pesan.js
+++ js/pesan.js
@@ -18,3 +18,28 @@ function konfirmasiHapus(id, nama) {
     }
   });
 }
+function confirmDelete(event, element) {
+  event.preventDefault(); // Mencegah langsung ke link
+
+  Swal.fire({
+    title: "Yakin ingin menghapus?",
+    text: "Data yang dihapus tidak bisa dikembalikan!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Ya, hapus!",
+    cancelButtonText: "Batal",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = element.href; // Lanjutkan ke link hapus
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+    }
+  });
+
+  return false; // Mencegah action default
+}
